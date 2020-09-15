81f5a78d6e3fa3356fb493712e0a36bff1b6b79d
chentaiyu0336 <2868510447@qq.com>
2020-09-15 18:06:06 +0800

    [cty] add fetch to connect back-end

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import "./style/index.scss";
+
+fetch("http://localhost:8080/users/1")
+  .then((response) => {
+    return response.ok
+      ? Promise.resolve(response.json())
+      : Promise.reject(response.json());
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
