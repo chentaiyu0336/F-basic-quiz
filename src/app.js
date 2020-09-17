bf786d5bf8c5db69df2b5a4189232f92662f4da5
杜娟 <jdu.xian@gmail.com>
2020-09-17 15:26:58 +0800

    feedback from Du Juan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "./style/index.scss";
 
 const url = window.location.href;
+// TODO feedback: 逻辑有漏洞，如果URL是/any/1，也是可以通过的
 const id = url.split("users/")[1];
 
 fetch(`http://localhost:8080/users/${id}`)
@@ -10,6 +11,7 @@ fetch(`http://localhost:8080/users/${id}`)
       : Promise.reject(response.json());
   })
   .then((data) => {
+    // TODO feedback: 方法太长了。。。考虑拆分
     const img = document.querySelector("img");
     img.src = data.avatar;
 
@@ -22,6 +24,7 @@ fetch(`http://localhost:8080/users/${id}`)
     aboutMsg.textContent = data.description;
     about.appendChild(aboutMsg);
 
+    // TODO feedback: education数据是一个单独的请求
     const { educationList } = data;
     const education = document.querySelector(".education");
     educationList.forEach((element) => {
@@ -29,7 +32,7 @@ fetch(`http://localhost:8080/users/${id}`)
 
       const eduInfo = document.createElement("div");
       eduInfo.className = "edu-info";
-
+      // TODO feedback: education用ul li更符合语义
       eduInfo.innerHTML = `
       <span class="year">${year}</span>
         <div class="education-message">
